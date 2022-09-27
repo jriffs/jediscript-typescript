338ca5ca66a9a2e0f4b31ff503734abbe4b3666e
Jedi of Javascript <40812932+jriffs@users.noreply.github.com>
2022-09-27 07:07:16 +0100

    updated something

--- src/jediscript.ts
+++ src/jediscript.ts
@@ -3,7 +3,7 @@ import { app, checkSemantic, getProperties } from "./worker-functions/workers.js
 import { ETwitterStreamEvent } from "twitter-api-v2"
 
 
-// http://localhost:5000/files/89b3dac1-e67e-466d-92fb-5300ab079721/url?filename=my-1664060629763-audio.mp3
+// http://localhost:5000/files/8decc08a-a0a1-4ad8-b800-860fbc68076a/url?filename=my-1664258701569-audio.mp3
 
 async function get_Stream() {
     try {
